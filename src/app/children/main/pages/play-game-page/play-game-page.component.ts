2854d4862943bf00483d21a8ddba9d59e5b48c20
Dmitry <dmitriy_kozachenko_02@mail.ru>
2023-11-23 15:21:21 +0500

    Add basic play game

--- src/app/children/main/pages/play-game-page/play-game-page.component.ts
+++ src/app/children/main/pages/play-game-page/play-game-page.component.ts
@@ -1,8 +1,17 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit, signal, WritableSignal } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    computed,
+    Inject,
+    OnInit,
+    Signal,
+    signal
+} from '@angular/core';
 import { Observable } from 'rxjs';
 import { GameDataService } from '../../data/services/game-data.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { BACKEND_URL_TOKEN } from '../../../../data/tokens/backend-url.token';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
     selector: 'play-game-page',
@@ -12,11 +21,12 @@ import { BACKEND_URL_TOKEN } from '../../../../data/tokens/backend-url.token';
 })
 export class PlayGamePageComponent implements OnInit {
     public games$: Observable<any> = new Observable<any>();
-    public gameId: WritableSignal<string> = signal('');
+    public gameUrl: Signal<SafeResourceUrl> = signal('');
     constructor(
         private _gameDataService: GameDataService,
         private _route: ActivatedRoute,
-        @Inject(BACKEND_URL_TOKEN) public readonly backendUrl: string
+        @Inject(BACKEND_URL_TOKEN) public readonly backendUrl: string,
+        private _sanitizer: DomSanitizer
     ) { }
 
     //TODO: реализовать отписку
@@ -25,7 +35,9 @@ export class PlayGamePageComponent implements OnInit {
         this._gameDataService.getAllGames().subscribe();
         this._route.params
             .subscribe((params: Params) => {
-                this.gameId.set(params['gameId']);
+                this.gameUrl = computed(() =>
+                    this._sanitizer.bypassSecurityTrustResourceUrl(`http://localhost:5000/games/${params['gameId']}/index.html`)
+                );
             });
     }
 }
