b33eba466228a970bda5cc864cc82f799ada261a
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-01-04 12:50:57 +0500

    Add environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { IEnvironment } from './environment.interface';
+
+export const environment: IEnvironment = {
+    production: true,
+    apiUrl: 'http://localhost:5000' //TODO: Здесь нужно будет добавить адрес удаленного сервера
+};
