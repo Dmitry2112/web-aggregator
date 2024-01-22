02c592070d4ae7bd35a67a9937ba1dafdcf7e308
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-01-22 18:19:01 +0500

    Delete backend-url token from app module providers

--- src/app/data/tokens/backend-url.token.ts
+++ src/app/data/tokens/backend-url.token.ts
@@ -1,3 +1,9 @@
 import { InjectionToken } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
-export const BACKEND_URL_TOKEN: InjectionToken<string> = new InjectionToken<string>('url for making api requests');
+export const BACKEND_URL_TOKEN: InjectionToken<string> = new InjectionToken<string>(
+    'url for making api requests',
+    {
+        factory: () => environment.apiUrl
+    }
+);
