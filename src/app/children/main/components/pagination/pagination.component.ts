dcd26509963038371ad828a53ff99bfe2836d2dc
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-04-09 18:04:27 +0500

    feat: add disabled style pagination

--- src/app/children/main/components/pagination/pagination.component.ts
+++ src/app/children/main/components/pagination/pagination.component.ts
@@ -1,12 +1,12 @@
 import {
     ChangeDetectionStrategy,
-    Component,
+    Component, computed,
     input,
-    InputSignal, OnInit, signal, WritableSignal,
+    InputSignal, OnInit, Signal, signal, WritableSignal,
 } from '@angular/core';
 import { PaginationService } from '../../services/pagination.service';
 import { BehaviorSubject, distinctUntilChanged, takeUntil, tap } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgClass } from '@angular/common';
 import { TuiDestroyService } from '@taiga-ui/cdk';
 import { FilterService } from '../../services/filter.service';
 
@@ -14,7 +14,8 @@ import { FilterService } from '../../services/filter.service';
     selector: 'pagination',
     standalone: true,
     imports: [
-        AsyncPipe
+        AsyncPipe,
+        NgClass
     ],
     providers: [TuiDestroyService],
     templateUrl: './pagination.component.html',
@@ -22,11 +23,17 @@ import { FilterService } from '../../services/filter.service';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PaginationComponent implements OnInit {
+    // TODO: решить проблему с подпиской на текущую страницу
+
     public gamesOnPage: InputSignal<number> = input.required<number>();
+
     public gamesCount$: BehaviorSubject<number> = this._filterService.gamesCount$;
     public pagesCount: WritableSignal<number> = signal(0);
-
     public currentPage$: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+
+    public curPage: WritableSignal<number> = signal(1);
+    public leftDisabled: Signal<boolean> = computed(() => this.curPage() === 1);
+    public rightDisabled: Signal<boolean> = computed(() => this.curPage() === this.pagesCount());
 
     constructor(
         private _paginationService: PaginationService,
@@ -52,10 +59,12 @@ export class PaginationComponent implements OnInit {
     }
 
     public moveLeft(): void {
+        this.curPage.update((value: number) => value === 1 ? value : value - 1);
         this.currentPage$.next(this.currentPage$.value === 1 ? this.currentPage$.value : this.currentPage$.value - 1);
     }
 
     public moveRight(): void {
+        this.curPage.update((value: number) => value === this.pagesCount() ? value : value + 1);
         this.currentPage$.next(this.currentPage$.value === this.pagesCount() ? this.currentPage$.value : this.currentPage$.value + 1);
     }
 }
