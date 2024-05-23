d242503869c4cfc4f52e2570a4ed69267ab14993
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-05-23 13:28:07 +0500

    feat: use object storage for default user avatar

--- src/app/children/main/components/main-header/main-header.component.ts
+++ src/app/children/main/components/main-header/main-header.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, computed, Signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, OnInit, signal, Signal } from '@angular/core';
 import { AuthService } from '../../children/auth/data/services/auth.service';
 import { NgIf, NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
     selector: 'main-header',
@@ -11,11 +12,20 @@ import { RouterLink } from '@angular/router';
     standalone: true,
     imports: [RouterLink, NgIf, NgClass]
 })
-export class MainHeaderComponent {
+export class MainHeaderComponent implements OnInit {
     public showMenu: boolean = false;
     public isLogin: Signal<boolean> = computed(() => this._authService.isAuthenticated());
+    public defaultAvatarUrl: Signal<SafeResourceUrl> = signal('');
 
-    constructor(private _authService: AuthService) {
+    constructor(
+        private _authService: AuthService,
+        private _sanitizer: DomSanitizer
+    ) {}
+
+    public ngOnInit(): void {
+        this.defaultAvatarUrl = computed(() =>
+            this._sanitizer.bypassSecurityTrustResourceUrl('https://storage.yandexcloud.net/web-aggregator-bucket/avatar-guest-img.svg')
+        );
     }
 
     public show(): void {
